04139b5b303a36de1018a85e419e6a792784f280
Paweł Galias <pawgalias@gmail.com>
2020-01-26 00:13:17 +0100

    feature(spinner): create loading spinner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RowDirective } from './directives/row.directive';
 import { ColumnDirective } from './directives/column.directive';
 import { InputWrapperComponent } from './components/input-wrapper/input-wrapper.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     RowDirective,
     ColumnDirective,
     InputWrapperComponent,
+    SpinnerComponent,
   ],
   exports: [
     RowDirective,
     ColumnDirective,
     InputWrapperComponent,
+    SpinnerComponent,
   ],
   imports: [
     CommonModule
